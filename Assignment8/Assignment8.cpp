3e36fa858f1ff73f33569499aa2f97d59c992a24
Atharva Joshi <athu200299@gmail.com>
2019-04-18 03:24:28 +0530

    Dijkstra Correction
    Changed Code was working only on the first Vertex of the graph

--- Assignment8/Assignment8.cpp
+++ Assignment8/Assignment8.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 #include<iostream>
 #define V 5
+#define infi 100000
 using namespace std;
 struct Graph
 {
@@ -18,34 +19,38 @@ int relaxation(int u,int v,Graph g[V][V])
 
 int find_min(Graph g[V][V])
 {
-	int min = INT_MAX,index =-1,i;
+	int min = infi,index =-1,i;
 	for( i = 1;i < V;i++)
 	{
-		if(g[i][i].label < min && g[i][i].prev == -1 && g[i][i].label != INT_MAX)
+		if(g[i][i].label < min && g[i][i].prev == -1 && g[i][i].label != infi){
 			min = g[i][i].label;
 			index = i;
+		}
 	}
-	return i;
+	cout<<"Min Lablel :"<<min<<"\n";
+	return index;
 }
 
 void shortest_path(int start,Graph g[V][V])
 {
 	for(int i = 1;i<V;i++)
 	{
-		g[i][i].label = INT_MAX;
+		g[i][i].label = infi;
 		g[i][i].prev = -1;
 	}
 	g[start][start].label = 0;
-	int previous_vertex = -1;
+	int previous_vertex = start;
 	for(int i = 1 ;i < V; i++)
 	{
 		int u = find_min(g);
-		g[u][u].prev = previous_vertex;
+		cout<<"Selected Vertex ID(Check Name Array):"<<u<<"\n\n\n";
+
 		for(int j = 1;j<V ;j++)
 		{
-			if(g[i][j].weight != 0 && g[j][j].prev == -1)
-				g[j][j].label = relaxation(i,j,g);
+			if(g[u][j].weight != infi && g[j][j].prev == -1)
+				g[j][j].label = relaxation(u,j,g);
 		}
+		g[u][u].prev = previous_vertex;
 		previous_vertex = u;
 	}
 }
@@ -61,8 +66,10 @@ void createConnections(Graph g[V][V],char name[V][25])
 		for(int j = 1;j < V;j++)
 		{
 			if(i != j){
-				cout<<"Enter Weight for "<<name[i]<<" - "<<name[j]<<" : ";
+				cout<<"Enter Weight for "<<name[i]<<" - "<<name[j]<<" (Press 0 if not edge exists): ";
 				cin>>g[i][j].weight;
+				if(g[i][j].weight == 0)
+					g[i][j].weight = infi;
 			}
 			else
 				g[i][j].weight = 0;
